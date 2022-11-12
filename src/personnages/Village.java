b463446c5f9cf881a2e1db0c75bbb76c9a205516
unknown <adrianm2308@gmail.com>
2022-11-12 12:19:01 +0100

    TP3

--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -2,12 +2,12 @@ package personnages;
 
 public class Village {
 	private String nom;
-	private static Chef chef;
+	private String chef;
 	private static int[] villagois;
 	private static int nbVillagois = 0;
 	
 	
-	public Village(String nom, Chef chef, int nbVillagoisMaximum) {
+	public Village(String nom, String chef, int nbVillagoisMaximum) {
 		this.nom = nom;
 		this.chef = chef;
 		villagois = new int[nbVillagoisMaximum];
@@ -22,18 +22,30 @@ public class Village {
 		return nom;
 	}
 	
-	public void setChef(Chef chef) {
+	public void setChef(String chef) {
 		this.chef = chef;
 	}
 	public int trouverHabitant(int numero) {
 		return  villagois[numero];
 	}
 	
+	public void afficherVillagois() {
+		System.out.println("Dans le village du chef" + chef + " vivent les légendaires gaulois :");
+		int i = 0;
+		while (i<3) {
+			System.out.println(" - "+ villagois[i]);
+			++i;
+		}
+	}
+	
 	public static void main(String[] args) {
-		Village village = new Village( " Village des Irréductibles", chef, 30);
 		Gaulois abracourcix = new Gaulois( "Abracourcix",6);
-		Chef abracourcix;
+		Village village = new Village( " Village des Irréductibles", "Abracourcix", 30);
 		ajouterHabitant(abracourcix);
+		Gaulois asterix = new Gaulois( "Astérix",8);
+		ajouterHabitant(asterix);
+		Gaulois obelix = new Gaulois( "Obélix",25);
+		ajouterHabitant(obelix);
 	}
 	
 }
